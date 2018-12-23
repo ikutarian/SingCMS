af3f2339651c4c39363e8a09407de92a094f9c5a
ikutarian <saitou0217@outlook.com>
2018-12-23 15:12:43 +0800

    init project

---
+++ src/main/java/com/ikutarian/singcms/SingCmsApplication.java
@@ -0,0 +1,14 @@
+package com.ikutarian.singcms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingCmsApplication.class, args);
+	}
+
+}
+
